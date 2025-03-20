b6a9e631fc36d0183001c8b9c27c20a3aad73ac3
yunzivv <yunzivv@gmail.com>
2025-03-20 15:41:26 +0900

    2번문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int width = sc.nextInt();
-        int height = sc.nextInt();
+        int step = sc.nextInt();
+        int j = 0; // 공백 개수를 제어하는 변수
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                System.out.print("*");
+        for (int line = step; line >= 1; line--) { // 입력 받은 수만큼 반복
+
+            for (int n = 0; n <= j; n++){ // 공백 출력 반복문
+                System.out.print(" ");
             }
-            System.out.print("\n");
+            j++;
+
+            for (int decNum = line; decNum >= 1; decNum--) { // 숫자 출력 반복문
+                System.out.print(decNum + " ");
+            }
+            System.out.println(""); // 개행을 위한 출력
         }
     }
 }
