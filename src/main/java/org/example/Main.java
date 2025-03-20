e6ccdd6412ad7759ff34bba0b2499582d941bb03
yunzivv <yunzivv@gmail.com>
2025-03-20 15:54:39 +0900

    3번문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int step = sc.nextInt();
-        int j = 0; // 공백 개수를 제어하는 변수
+        int next = 1;
+        int now = 0;
+        int temp;
 
-        for (int line = step; line >= 1; line--) { // 입력 받은 수만큼 반복
-
-            for (int n = 0; n <= j; n++){ // 공백 출력 반복문
-                System.out.print(" ");
-            }
-            j++;
-
-            for (int decNum = line; decNum >= 1; decNum--) { // 숫자 출력 반복문
-                System.out.print(decNum + " ");
-            }
-            System.out.println(""); // 개행을 위한 출력
+        for(int i = 0; i < 10; i++){
+            System.out.println(now);
+            temp = now;
+            now = now + next;
+            next = temp;
         }
     }
 }
